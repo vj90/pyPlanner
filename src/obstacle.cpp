fe0831f0b32334b27608aae3ff20a3ac6b40b202
Vasundhara <vasundharajain1990@gmail.com>
2025-02-07 16:12:07 +0100

    Add obstacle class

---
+++ src/obstacle.cpp
@@ -0,0 +1,23 @@
+#include "obstacle.h"
+
+Obstacle::Obstacle() {}
+
+Obstacle::Obstacle(float x, float y) : center_x(x), center_y(y) {}
+
+void Obstacle::setCenter(float x, float y) {
+  center_x = x;
+  center_y = y;
+}
+
+std::pair<float, float> Obstacle::getCenter() { return {center_x, center_y}; }
+
+CircularObstacle::CircularObstacle(float x, float y, float radius)
+    : Obstacle(x, y), radius(radius) {}
+
+float CircularObstacle::getRadius() { return radius; }
+
+void CircularObstacle::setRadius(float radius) { this->radius = radius; }
+
+bool CircularObstacle::collision(std::vector<RobotConfig> path_samples) {
+  return false;
+}
---
+++ src/obstacle.h
@@ -0,0 +1,33 @@
+#ifndef OBSTACLE_H
+#define OBSTACLE_H
+
+#include <cmath>
+#include <vector>
+
+#include "RobotConfig.h"
+
+class Obstacle {
+ public:
+  Obstacle();
+  Obstacle(float x, float y);
+  void setCenter(float x, float y);
+  std::pair<float, float> Obstacle::getCenter();
+  virtual bool collision(std::vector<RobotConfig> path_samples) = 0;
+
+ private:
+  float center_x{NAN};
+  float center_y{NAN};
+};
+
+class CircularObstacle : public Obstacle {
+ public:
+  CircularObstacle(float x, float y, float radius);
+  float getRadius();
+  void setRadius(float radius);
+  bool collision(std::vector<RobotConfig> path_samples) override;
+
+ private:
+  float radius{NAN};
+};
+
+#endif
